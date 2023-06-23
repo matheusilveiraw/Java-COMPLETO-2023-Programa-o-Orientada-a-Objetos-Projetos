1c3f539da44a8018bfab287630483cc411fe840c
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2023-06-23 10:07:45 -0300

    Add files via upload

---
+++ 8-66-segundo exemplo/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+		
+import entities.product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		product product = new product();
+		
+		System.out.println("Enter product data: ");
+		System.out.println("- Name: ");
+		product.name = sc.nextLine();
+		System.out.println("- Price: ");
+		product.price = sc.nextDouble();
+		System.out.println("- Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+	}
+
+}
