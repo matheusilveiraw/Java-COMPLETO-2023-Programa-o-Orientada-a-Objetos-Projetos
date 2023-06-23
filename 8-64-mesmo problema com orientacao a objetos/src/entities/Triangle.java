d9455cc205b36e9c87d331176c2e2d30ce955747
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2023-06-23 09:24:15 -0300

    Add files via upload

--- 8-64-mesmo problema com orientacao a objetos/src/entities/Triangle.java
+++ 8-64-mesmo problema com orientacao a objetos/src/entities/Triangle.java
@@ -6,5 +6,13 @@ public class Triangle {
 	public double b;
 	public double c; 
 	
-	
+	public double area() {
+		//public para ser usada em outros lugares
+		//double pq vai retornar um double 
+		//area nome da função 
+		//entre parenteses parametros
+		
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p * c));
+	}
 }
