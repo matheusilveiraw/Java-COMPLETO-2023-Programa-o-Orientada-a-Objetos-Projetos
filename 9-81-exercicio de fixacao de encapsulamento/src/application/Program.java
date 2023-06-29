e8751f2cf37b87ca5c9292a36d3044b9dbf53c87
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2023-06-29 13:32:49 -0300

    Add files via upload

--- 9-81-exercicio de fixacao de encapsulamento/src/application/Program.java
+++ 9-81-exercicio de fixacao de encapsulamento/src/application/Program.java
@@ -1,21 +1,44 @@
 package application;
 
 import java.util.Scanner;
+import entities.Conta;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		double initialDepositValue = 0;
+		char initialDepositPermission;
+		String accHolder;
+		int accNumber;
+						
 		System.out.println("Enter account number: ");
-		int accNumber = sc.nextInt();
+		accNumber = sc.nextInt();
 		System.out.println("Enter account holder: ");
-		String accHolder = sc.next();
+		accHolder = sc.next();
 		System.out.println("Is there a initial deposit? (Y/N)");
-		char initialDeposit = sc.next().charAt(0); ///tem que usar o char at aqui 
-		System.out.println("Enter initial deposit value: ");
-		double initialDepositValue = sc.nextDouble();
+		initialDepositPermission = sc.next().charAt(0);
+		if(initialDepositPermission == 'Y') {
+			System.out.println("Enter initial deposit value: ");
+			initialDepositValue = sc.nextDouble();
+		}
 		
-		System.out.println(accNumber + " " + accHolder + " " + initialDeposit + " " + initialDepositValue);
+		Conta conta = new Conta(accNumber, accHolder, initialDepositValue);
+		
+		System.out.println("Holder: " + conta.getAccHolder() + ", Acc: "+ conta.getAccNumber() + ",  Balance: R$" + conta.getTotalValue());
+		
+		System.out.println("Enter a deposit value: ");
+		initialDepositValue = sc.nextDouble();
+		conta.deposito(initialDepositValue);
+		
+		System.out.println("Holder: " + conta.getAccHolder() + ", Acc: "+ conta.getAccNumber() + ",  Balance: R$" + conta.getTotalValue());
+
+		
+		System.out.println("Enter a withdraw value: ");
+		initialDepositValue = sc.nextDouble();
+		conta.saque(initialDepositValue);
+		
+		System.out.println("Holder: " + conta.getAccHolder() + ", Acc: "+ conta.getAccNumber() + ",  Balance: R$" + conta.getTotalValue());
 				
 		sc.close();
 	}
--- 9-81-exercicio de fixacao de encapsulamento/src/entities/Conta.java
+++ 9-81-exercicio de fixacao de encapsulamento/src/entities/Conta.java
@@ -1,5 +1,45 @@
 package entities;
 
 public class Conta {
+	private String accHolder;
+	private int accNumber;
+	private double totalValue;
+	
+	public void deposito(double value) {
+		this.totalValue += value;
+	}
+	
+	public void saque(double value) {
+		this.totalValue = this.totalValue - value - 5;
+	}
+	
+	
+	public Conta(int accNumber, String accHolder, double totalValue) {
+		super();
+		this.accHolder = accHolder;
+		this.accNumber = accNumber;
+		this.totalValue = totalValue;
+	}
 
+	//-------------------
+
+	public String getAccHolder() {
+		return accHolder;
+	}
+
+	public void setAccHolder(String accHolder) {
+		this.accHolder = accHolder;
+	}
+
+	public int getAccNumber() {
+		return accNumber;
+	}
+
+	public double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(double totalValue) {
+		this.totalValue = totalValue;
+	}
 }
